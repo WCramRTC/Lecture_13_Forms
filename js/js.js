64eb61dcb8f3d6965b955cd48829e6469cca0ec2
WCram <wcram@rtc.edu>
2022-02-24 11:28:32 -0800

    Updated Examples

--- js/js.js
+++ js/js.js
@@ -1,13 +1,17 @@
-const userInput_1 = document.getElementById('fName');
-const userInput_2 = document.getElementById('lName');
-const btnDispaly = document.getElementById('btnDisplay');
+const userInput_1 = document.getElementById('value-1');
+const userInput_2 = document.getElementById('value-2');
+const display_result = document.getElementById('txtDisplay');
+const btnDispaly = document.getElementById('btnAdd');
 
 
 
-btnDispaly.addEventListener('click', function() {
-    let value1 = parseInt(userInput_1.value);
+btnDispaly.addEventListener('click', AddValues);
+
+function AddValues() {
+
+let value1 = parseInt(userInput_1.value);
 let value2 = parseInt(userInput_2.value);
+let addedValues = value1 + value2;
+display_result.value = addedValues;
 
-alert(value1 + value2);
-
-})
+}
